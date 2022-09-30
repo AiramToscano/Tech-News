e7301437d314c45934456335c7f96447c91feefa
Airam Toscano <airamlobato@gmail.com>
2022-09-29 21:29:16 -0400

    Feat: search_by_date()

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,6 @@
+
 from ..database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -15,7 +17,25 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        listTitle = []
+        d1 = datetime.strptime(date, '%Y-%m-%d')
+        dataFormat = str(d1).split()[0]
+        data = dataFormat.split('-')
+        dataformatCorrect = f"{data[2]}/{data[1]}/{data[0]}"
+        db = search_news({"timestamp": {"$regex": dataformatCorrect}})
+        if(db == []):
+            listTitle.append([])
+            return []
+        for i in db:
+            tupla = (i["title"], i["url"])
+            listTitle.append(tupla)
+            return listTitle
+    except ValueError:
+        raise ValueError('Data inválida')
+
+
+# print(search_by_date('2022-09-28'))
 
 
 # Requisito 8
